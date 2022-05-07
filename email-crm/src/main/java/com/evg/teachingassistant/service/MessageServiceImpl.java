f534616295ba656b93d781ea4c2c811e7b4d3f09
evgeny <lolss1338@gmail.com>
2022-05-07 12:36:36 +0300

    Fixed nodejs endpoint & cleared import

--- email-crm/src/main/java/com/evg/teachingassistant/service/MessageServiceImpl.java
+++ email-crm/src/main/java/com/evg/teachingassistant/service/MessageServiceImpl.java
@@ -18,15 +18,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
--- email-receiver-service/src/main/controller/EmailController.js
+++ email-receiver-service/src/main/controller/EmailController.js
@@ -4,22 +4,19 @@ import MessageService from "../service/MessageService.js";
 
 const router = Router()
 
-router.post("/api/v1/email/", (req, res) => {
+router.post("/api/v1/email/", (req, res, next) => {
     res.header("Content-Type", "application/json")
-    if (req.body.email != null && req.body.password != null && req.body.host != null) {
-        EmailReceiver.getAllEmails(req.body.email, req.body.password, req.body.host)
-            .then(data => {
-                let messageList = {
-                    messageList: data
-                }
-                res.json(messageList)
-            })
-    }
-    // res.sendStatus(400)
+    EmailReceiver.getAllEmails(req.body.email, req.body.password, req.body.host, next)
+        .then(data => {
+            let messageList = {
+                messageList: data
+            }
+            res.json(messageList)
+        }).catch(next)
 })
 
-router.get("/api/v1/email/file/:fileId", (req, res,next) => {
-    MessageService.getFileById(req.params.fileId, res,next)
+router.get("/api/v1/email/file/:fileId", (req, res, next) => {
+    MessageService.getFileById(req.params.fileId, res, next)
 })
 
 export default router
